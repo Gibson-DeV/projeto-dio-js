95b776bd428a33becec8bb725f6bc34cbe82cfd4
Celebrimbor02 <gibson8fla@gmail.com>
2023-11-23 22:04:17 -0300

    Refatoracao

--- assets/js/poker-api.js
+++ assets/js/poker-api.js
@@ -4,19 +4,17 @@ const pokerApi = {}
 
 pokerApi.getPokemonsDetails = (pokemon) => {
     return fetch(pokemon.url)
-            .then((responseDetailsPokemons) => responseDetailsPokemons.json())
+            .then((responsePokemonUrl) => responsePokemonUrl.json())
 }
 
 //Consumo da API, converte em JSON e retorna o atributo results.  
 pokerApi.getPokemons = (offset = 0, limit = 5) => {
-
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
-
     return fetch(url)
         .then((response) => response.json())
         .then((responseJson) => responseJson.results)
         .then((pokemonsList) => pokemonsList.map(pokerApi.getPokemonsDetails))
-        .then((pokemonDetails) => Promise.all(pokemonDetails))
+        .then((pokemonDetailsJson) => Promise.all(pokemonDetailsJson))
         .then((pokemonDetails) => pokemonDetails)
-        .catch((error) => console.error(error));
+        .catch(console.error);
 };
